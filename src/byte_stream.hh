7923b8191224371d80040dc831fa9077af885a7f
wangpan <2232852166@qq.com>
2023-08-14 13:45:04 +0000

    lab1 有别人的参考代码

--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -19,6 +19,19 @@ protected:
   int writeByte;
   int readByte;
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
+/*uint64_t capacity_;
+  // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
+  bool is_closed_ { false };
+  bool has_error_ { false };
+
+  uint64_t num_bytes_pushed_ { 0 };
+  uint64_t num_bytes_popped_ { 0 };
+  uint64_t num_bytes_buffered_ { 0 };
+
+  std::deque<std::string> data_queue_ {};
+  std::deque<std::string_view> view_queue_ {};
+*/
+
 
 public:
   explicit ByteStream( uint64_t capacity );
