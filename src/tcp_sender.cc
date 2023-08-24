e620c867d228a3a112782c60f60e7533138d54c3
wangpan <2232852166@qq.com>
2023-08-24 14:21:43 +0000

    lab3完成

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -13,41 +13,113 @@ TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  return outstanding_cnt;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return retransmit_cnt_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-  // Your code here.
-  return {};
+  if (queued_segments.empty())
+    return {};
+
+  if (!timer.is_running())
+    timer.start();
+
+  TCPSenderMessage msg = queued_segments.front();
+  queued_segments.pop();
+
+  return msg;
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+  uint64_t currwindow_size = window_size != 0 ? window_size : 1;
+  while (outstanding_cnt < currwindow_size) {
+    TCPSenderMessage msg;
+    msg.seqno = Wrap32::wrap(next_seqno, isn_);
+
+    if (!syn_) {
+      msg.SYN = syn_ = true;
+      outstanding_cnt += 1;
+    }
+
+    uint64_t payload_size = min(TCPConfig::MAX_PAYLOAD_SIZE, currwindow_size - outstanding_cnt);
+    read(outbound_stream, payload_size, msg.payload);
+    outstanding_cnt += msg.payload.size();
+
+    if (!fin_ && outbound_stream.is_finished() && outstanding_cnt < currwindow_size) {
+      fin_ = true;
+      msg.FIN = true;
+      outstanding_cnt += 1;
+    }
+
+    if (msg.sequence_length() == 0)
+      break;
+
+    queued_segments.push(msg);
+    outstanding_segments.push(msg);
+    next_seqno += msg.sequence_length();
+
+    if (msg.FIN || outbound_stream.bytes_buffered() == 0)
+      break;
+  }
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
   // Your code here.
-  return {};
+  auto seqno = Wrap32::wrap(next_seqno, isn_);
+  return {seqno, false, {}, false};
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  window_size = msg.window_size;
+  if (msg.ackno.has_value()) {
+    auto ackno = msg.ackno.value().unwrap(isn_, next_seqno);
+
+    if (ackno > next_seqno)
+      return;
+
+    acked_seqno = ackno;
+
+    while (!outstanding_segments.empty()) {
+      auto front_msg = outstanding_segments.front();
+      if (front_msg.sequence_length() + front_msg.seqno.unwrap(isn_, next_seqno) <= acked_seqno) {
+        outstanding_segments.pop();
+        outstanding_cnt -= front_msg.sequence_length();
+
+        timer.resetRTO();
+        if (!outstanding_segments.empty()) {
+          timer.start();
+        }
+        retransmit_cnt_ = 0;
+      } else {
+        break;
+      }
+    }
+
+    if (outstanding_segments.empty())
+      timer.stop();
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  timer.tick(ms_since_last_tick);
+  if (timer.is_expired()) {
+    queued_segments.push(outstanding_segments.front());
+    if (window_size != 0) {
+      ++retransmit_cnt_;
+      timer.doubleRTO();
+    }
+
+    timer.start();
+  }
+
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,11 +3,72 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include <queue>
+
+using namespace std;
+
+class Timer{
+private:
+  uint64_t initial_RTO_ms;
+  uint64_t current_RTO_ms;
+  uint64_t time_ms {0};
+  bool running {false};
+
+public:
+
+  explicit Timer(uint64_t init_ROT) : initial_RTO_ms(init_ROT), current_RTO_ms(init_ROT) {}
+
+  void stop() {
+    running = false;
+  }
+
+  void start() {
+    time_ms = 0;
+    running = true;
+  }
+
+  void tick(uint64_t ms_since_last_tick) {
+    if (running) {
+      time_ms += ms_since_last_tick;
+    }
+  }
+
+  bool is_running() {
+    return running;
+  }
+
+  bool is_expired() {
+    return running && time_ms >= current_RTO_ms;
+  }
+
+  void doubleRTO() {
+    current_RTO_ms *= 2;
+  }
+
+  void resetRTO() {
+    current_RTO_ms = initial_RTO_ms;
+  }
+
+};
 
 class TCPSender
 {
   Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+  uint64_t initial_RTO_ms_;//超时时长
+
+  bool syn_ {false};
+  bool fin_ {false};
+  uint64_t retransmit_cnt_ {0};
+
+  uint64_t acked_seqno {0};
+  uint64_t next_seqno {0};
+  uint64_t window_size {1};
+  uint64_t outstanding_cnt {0};
+
+  queue<TCPSenderMessage> outstanding_segments {};
+  queue<TCPSenderMessage> queued_segments {};
+
+  Timer timer {initial_RTO_ms_};
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
